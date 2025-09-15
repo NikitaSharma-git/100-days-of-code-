1f96c36818da8f002d4ab7ba6c752d405d925910
NikitaSharma-git <nikitasharma21360@gmail.com>
2025-09-15 17:14:30 +0530

    first commit

---
+++ Day13/num.c
@@ -0,0 +1,20 @@
+//Write a program to print numbers from 1 to n.
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    // Input 
+    printf("Enter the value of n: ");
+    scanf("%d", &n);
+
+    for (i = 1; i <= n; i++) {
+        printf("%d ", i);
+    }
+
+    printf("\n");
+    return 0;
+}
+
+
+
