f037f708ced67955e382a3f1c1947174dbb729c8
NikitaSharma-git <nikitasharma21360@gmail.com>
2025-09-19 21:20:04 +0530

    first commimt

---
+++ Day30/element.c
@@ -0,0 +1,32 @@
+//Count positive, negative, and zero elements in an array.
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int pos = 0, neg = 0, zero = 0;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for(i = 0; i < n; i++) {
+        if(arr[i] > 0)
+            pos++;
+        else if(arr[i] < 0)
+            neg++;
+        else
+            zero++;
+    }
+
+    printf("Positive numbers = %d\n", pos);
+    printf("Negative numbers = %d\n", neg);
+    printf("Zeros = %d\n", zero);
+
+    return 0;
+}
