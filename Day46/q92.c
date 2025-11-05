9999a85603108b3122dda25147705a1c96c37880
NikitaSharma-git <nikitasharma21360@gmail.com>
2025-11-05 08:57:25 +0530

    first commit

---
+++ Day46/q92.c
@@ -0,0 +1,23 @@
+//Find the first repeating lowercase alphabet in a string.
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[26] = {0}, i;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] >= 'a' && str[i] <= 'z') {
+            freq[str[i] - 'a']++;
+            if(freq[str[i] - 'a'] == 2) {
+                printf("First repeating lowercase letter: %c\n", str[i]);
+                return 0;
+            }
+        }
+    }
+
+    printf("No repeating lowercase letter found.\n");
+    return 0;
+}
